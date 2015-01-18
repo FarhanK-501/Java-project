17536e6344c3add0193bb84e77f9c0e0dc3f6c4c
FarhanK-501 <i.typ3r@hotmail.com>
2015-01-18 11:33:29 +0500

    java project

---
+++ src/testing/Elementery.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class Elementery {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 System.out.println("Elementery! My dear Watson!");
+	}
+
+}
