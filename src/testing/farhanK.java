539aec5a3739cbe368e0f90de6d421633c84b96f
FarhanK-501 <FarhanK-501@users.noreply.github.com>
2015-01-18 12:13:38 +0500

    Update farhanK.java
    commiting through browser

--- src/testing/farhanK.java
+++ src/testing/farhanK.java
@@ -2,6 +2,10 @@ package testing;
 
 public class farhanK {
 public static void main(String args[]){
+	boolean itsHappening = true;
 	System.out.print("False but with contradiction");
+	if(itsHappening){
+		System.out.println("Ok ke report");
+	}
 }
 }
